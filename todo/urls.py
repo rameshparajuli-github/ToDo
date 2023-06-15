538d7101d41029c59aadda0d647be00f59a273ba
Ramesh Parajuli <rameshparajuli619@gmail.com>
2023-06-16 00:17:43 +1000

    ToDo App

--- todo/urls.py
+++ todo/urls.py
@@ -3,4 +3,16 @@ from . import views
 
 urlpatterns = [
     path('addTask/', views.addTask, name= 'addTask'),
+    # Mark as Done
+    path('mark_as_done/<int:pk>/', views.mark_as_done, name= 'mark_as_done'),
+    
+    # Mark as Undone
+    path('mark_as_undone/<int:pk>/', views.mark_as_undone, name= 'mark_as_undone' ),
+    # Edit Feature
+    path('edit_task/<int:pk>/', views.edit_task, name='edit_task'),
+    # Delete Task
+    path('delete_task/<int:pk>/', views.delete_task, name='delete_task'),
+
+    
+    
 ]
